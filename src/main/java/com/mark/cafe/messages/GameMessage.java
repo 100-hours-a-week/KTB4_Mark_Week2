9c14f96f1397ad54773938e11f1793f46bc4b3c4
juseungjuseung <juseungjuseung@gmail.com>
2026-05-22 23:41:01 +0900

    feat: 게임 메세지, 제조 메세지 추가

--- src/main/java/com/mark/cafe/messages/GameMessage.java
+++ src/main/java/com/mark/cafe/messages/GameMessage.java
@@ -9,7 +9,11 @@ public enum GameMessage implements Messages {
                 일정 금액 이상 매출을 달성하면 퇴근입니다.
                 일정 금액 이하로 매출이 떨어지면 쫓겨납니다."""),
     FAIL_GAME("매출을 너무 떨어트렸습니다. 쫓겨났습니다."),
-    SUCCESS_GAME("매출을 성공적으로 올렸습니다. 퇴근입니다. 축하합니다.");
+    SUCCESS_GAME("매출을 성공적으로 올렸습니다. 퇴근입니다. 축하합니다."),
+    DOWN_RATING("대기열이 가득차서 주문을 받지 못했습니다. 평점이 낮아졌습니다."),
+    UP_RATING("평점이 올라갔습니다."),
+    END_GAME("게임이 종료됐습니다"),
+    LOW_RATING("평점이 너무 낮습니다 쫓겨났습니다.");
 
 
     private String message;
--- src/main/java/com/mark/cafe/messages/MakingMessage.java
+++ src/main/java/com/mark/cafe/messages/MakingMessage.java
@@ -7,6 +7,7 @@ public enum MakingMessage implements Messages {
     SELECT_RECIPE_ORDER("제조 방식을 올바른 레시피 순서대로 골라주세요"),
     CHOICE_STEP("선택지를 골라주세요"),
     WRONG_CHOICE_STEP("제조중 잘못된 행동을 했습니다 돈을 잃습니다"),
+    LATE_MAKING("제조 시간이 지연되어 평점이 감소합니다"),
     FAILED_MAKING("제조 방법이 틀렸습니다 돈을 잃습니다."),
     SUCCESS_MAKING("메뉴 제조에 성공했습니다");
 
